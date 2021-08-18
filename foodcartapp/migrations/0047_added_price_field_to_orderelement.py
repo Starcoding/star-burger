ba81e4bdd6e49f7534c8b086a3c524283d3707b5
Starcoding <yury.starovoyt@gmail.com>
2021-08-18 23:23:54 +0300

    Added migrations for previous steps

---
+++ foodcartapp/migrations/0047_added_price_field_to_orderelement.py
@@ -0,0 +1,33 @@
+from django.db import migrations
+from django.db import models
+from django.core.validators import MinValueValidator
+
+def set_price(apps, schema_editor):
+    OrderElement = apps.get_model('foodcartapp', 'OrderElement')
+    Product = apps.get_model('foodcartapp', 'Product')
+
+    for element in OrderElement.objects.all():
+        element.price = Product.objects.get(name=element.product.name).price
+        element.save(update_fields=['price'])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('foodcartapp', '0046_alter_order_managers'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='OrderElement',
+            name='price',
+            field=models.DecimalField(
+                null=True,
+                verbose_name='цена',
+                validators=[MinValueValidator(0)],
+                max_digits=10,
+                decimal_places=2
+            )
+        ),
+        migrations.RunPython(set_price)
+    ]
