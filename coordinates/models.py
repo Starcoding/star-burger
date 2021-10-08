55c1dddd87a4f135f70c7f6b4bcf4df1897b98e8
Starcoding <yury.starovoyt@gmail.com>
2021-10-08 23:10:55 +0300

    Made field not necessary

--- coordinates/models.py
+++ coordinates/models.py
@@ -5,12 +5,14 @@ class Coordinates(models.Model):
     latitude = models.DecimalField(
         'широта',
         max_digits=11,
-        decimal_places=9
+        decimal_places=9,
+        null=True
     )
     longtitude = models.DecimalField(
         'долгота',
         max_digits=11,
-        decimal_places=9
+        decimal_places=9,
+        null=True
     )
     address = models.CharField(
         'адрес',
