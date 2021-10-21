7b031feaf20f4ba9dd9f1c54fd2f879facecfdf1
Starcoding <fagost32@gmail.com>
2021-10-21 17:01:10 +0300

    Added Rollbar notifications for errors

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -1,7 +1,10 @@
 import os
 
+import rollbar
 import dj_database_url
 from environs import Env
+from git import Repo
+
 
 env = Env()
 env.read_env()
@@ -12,6 +15,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 YANDEX_KEY = env('YANDEX_KEY')
 SECRET_KEY = env('SECRET_KEY', 'etirgvonenrfnoerngorenogneongg334g')
 DEBUG = env.bool('DEBUG', True)
+ROLLBAR_TOKEN = env('ROLLBAR_TOKEN')
+ROLLBAR_ENVIRONMENT = env('ROLLBAR_ENVIRONMENT', None)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
@@ -39,7 +44,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404'
 ]
+
+
+local_repo = Repo(path=BASE_DIR)
+local_branch = local_repo.active_branch.name
+ROLLBAR = {
+    'access_token': ROLLBAR_TOKEN,
+    'environment': ROLLBAR_ENVIRONMENT if ROLLBAR_ENVIRONMENT else f'{BASE_DIR}, {local_branch}, Debug:{DEBUG}',
+    'root': BASE_DIR,
+    'branch': local_branch,
+}
+rollbar.init(**ROLLBAR)
 
 ROOT_URLCONF = 'star_burger.urls'
 
